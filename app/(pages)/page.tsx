5a8b1554d0e1bb100fcea30c495a98a3157bc86b
Jorrit van der Heide <jorritvanderheide@pm.me>
2023-10-21 00:19:45 +0200

    omg

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -1,24 +1,34 @@
 import Header from "@/app/components/bars/Header";
 import Map from "@/app/components/maps/Map";
-import prisma from "@/app/lib/prisma/prisma";
+import { NextPage } from "next";
+import type { StoryProps } from "@/app/types/global.t";
 
 // Fetch stories
-async function getData() {
-  const res: any = await prisma.story.findMany();
+const getStories = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_FETCH_URL}/stories`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
 
-  return res;
-}
+  if (!res.ok) {
+    throw new Error("Failed to fetch stories");
+  }
+
+  return res.json();
+};
 
 // Export default
-export default async function Home() {
-  const data = await getData();
+const Home: NextPage = async () => {
+  const stories: StoryProps[] = await getStories();
 
   return (
     <>
       <main className="h-[100svh] w-full overflow-hidden">
         <Header />
-        {data && <Map stories={data} />}
+        {stories && <Map stories={stories} />}
       </main>
     </>
   );
-}
+};
+
+export default Home;
