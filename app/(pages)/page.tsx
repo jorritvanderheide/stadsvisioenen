f8439b3b659a42727e4710f7bac7e9d1988049a0
Jorrit van der Heide <jorritvanderheide@pm.me>
2023-10-20 22:55:43 +0200

    Force

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -26,7 +26,7 @@ const Home: NextPage = async () => {
     <>
       <main className="h-[100svh] w-full overflow-hidden">
         <Header />
-        <Map stories={stories} />
+        <Map stories={stories!} />
       </main>
     </>
   );
