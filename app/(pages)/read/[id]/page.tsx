96fc3bc60ebfbb21062589ead8aea86a9c7266e8
Jorrit van der Heide <jorritvanderheide@pm.me>
2023-10-20 21:14:36 +0200

    Add no store

--- app/(pages)/read/[id]/page.tsx
+++ app/(pages)/read/[id]/page.tsx
@@ -9,20 +9,20 @@ import Image from "next/image";
 import Link from "next/link";
 import AnimatedLink from "@/app/components/buttons/AnimatedLink";
 
-// /**
-//  * Generates static paths for all stories
-//  * @returns {any}
-//  */
-// export async function generateStaticParams() {
-//   const stories = await fetch(`${process.env.NEXT_PUBLIC_FETCH_URL}/stories`, {
-//     method: "GET",
-//     headers: { "Content-Type": "application/json" },
-//   }).then((res) => res.json());
+/**
+ * Generates static paths for all stories
+ * @returns {any}
+ */
+export async function generateStaticParams() {
+  const stories = await fetch(`${process.env.NEXT_PUBLIC_FETCH_URL}/stories`, {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  }).then((res) => res.json());
 
-//   return stories.map((story: StoryProps) => ({
-//     id: story.id,
-//   }));
-// }
+  return stories.map((story: StoryProps) => ({
+    id: story.id,
+  }));
+}
 
 // Fetches props for a single story
 async function getStory(id: string) {
@@ -71,7 +71,7 @@ const Read = async ({ params }: { params: { id: string } }) => {
       </div>
       <h1 className="text-h1 font-bold">Story not found</h1>
       <Image
-        className="absolute left-0 top-0 z-[-1] h-full object-cover opacity-80 blur-sm brightness-90"
+        className="w-fullobject-cover absolute left-0 top-0 z-[-1] h-full opacity-80 blur-sm brightness-90"
         src="/images/sign-in.webp"
         width={1573}
         height={1180}
