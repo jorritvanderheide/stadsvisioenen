b7deeff3bb7ec02d9ea9d4a3adb337d50f7c53bb
Jorrit van der Heide <jorritvanderheide@pm.me>
2023-10-21 06:27:33 +0200

    revalidate 0

--- app/(pages)/page.tsx
+++ app/(pages)/page.tsx
@@ -8,7 +8,7 @@ async function getStories() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_FETCH_URL}/api/stories`, {
     method: "GET",
     headers: { "Content-Type": "application/json" },
-    cache: "no-store",
+    next: { revalidate: 0 },
   });
 
   if (!res.ok) {
